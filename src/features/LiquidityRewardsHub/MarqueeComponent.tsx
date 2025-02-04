44e84e77709010e7c2eb1d1232480e4324519ac4
1Murad1 <muradabdulaev1998borec@gmail.com>
2025-02-04 13:43:39 +0200

    fix2

--- src/features/LiquidityRewardsHub/MarqueeComponent.tsx
+++ src/features/LiquidityRewardsHub/MarqueeComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { BoxIcon } from '../../ui/BoxIcon'
 import { styles } from '../../styles/styles'
 import { IMarqueeData } from '../../types/types'
